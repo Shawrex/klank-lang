a02b55bd08c588f236d38363a66985f8481cf532
Shawrex <38359132+Shawrex@users.noreply.github.com>
2021-09-27 10:29:57 +0200

    how do you fix stuff ?

--- klank.py
+++ klank.py
@@ -7,6 +7,7 @@ TT_INT =		'INTEGER'
 TT_STRING =		'STRING'
 TT_OP =			'OPERATOR'
 TT_KWORD =		'KEYWORD'
+TT_BRACK =		'BRACKETS'
 TT_SYMBOL =		'SYMBOL'
 TT_METHOD =		'METHOD'
 
@@ -15,13 +16,17 @@ TT_METHOD =		'METHOD'
 """
 LEXIC
 """
-Var = 'Var'
+Eol = ';'
+Var = 'var'
 Digits = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-Quote, Comparison, Then = '"', ':=', '>>'
+L_Paranth, R_Paranth = '(', ')'
+L_Curl, R_Curl = '{', '}'
+Quote, Comparison = '"', '=='
 Equal, Plus, Minus = '=', '+', '-'
-Print, Test = 'Print:', 'Test:'
+Print, If = 'print', 'if'
 
 vars = {}
+BannedChars = (Eol, L_Paranth, R_Paranth, L_Curl, R_Curl, Quote, Equal, Plus, Minus)
 
 
 
@@ -67,58 +72,85 @@ class Lexer(object):
 			if ch.isspace():
 				#skip spaces
 				self.skip_spaces()
+				continue
 
-			elif ch in Digits:
+			if ch == Eol:
+				self.step()
+				tokens.append(Token(TT_EOL, Eol))
+				continue
+				
+			if ch == Var[0]:
+				#Var ?
+				if (self.build_misc_name(Var)):
+					tokens.append(Token(TT_KWORD, Var))
+					continue
+
+			if ch == Print[0]:
+				#Print?
+				if (self.build_misc_name(Print)):
+					tokens.append(Token(TT_METHOD, Print))
+					continue
+
+			if ch == If[0]:
+				#Test ?
+				if (self.build_misc_name(If)):
+					tokens.append(Token(TT_METHOD, If))
+					continue
+			
+			if ch == Comparison[0]:
+				#Comparison ?
+				if (self.build_misc_name(Comparison)):
+					tokens.append(Token(TT_SYMBOL, Comparison))
+					continue
+
+			if ch in Digits:
 				#then character is an int token
 				tokens.append(Token(TT_INT, self.build_int()))
+				continue
 
-			elif ch in (Plus, Minus):
+			if ch in (Plus, Minus):
 				#token is an operator
 				self.step()
 				tokens.append(Token(TT_OP, ch))
+				continue
 
-			elif ch == Quote:
+			if ch == Quote:
 				#then character is a string token
 				tokens.append(Token(TT_STRING, self.build_string()))
+				continue
 			
-			elif ch == Equal:
+			if ch == Equal:
 				#equal token
 				self.step()
 				tokens.append(Token(TT_SYMBOL, Equal))
+				continue
+			
+			if ch == L_Paranth:
+				#left parathesis
+				self.step()
+				tokens.append(Token(TT_BRACK, L_Paranth))
+				continue
 				
-			elif ch == 'V':
-				#Var ?
-				if (self.build_misc_name(Var)):
-					self.step()
-					tokens.append(Token(TT_KWORD, Var))
-
-			elif ch == 'P':
-				#Print?
-				if (self.build_misc_name(Print)):
-					self.step()
-					tokens.append(Token(TT_METHOD, Print))
-
-			elif ch == 'T':
-				#Test ?
-				if (self.build_misc_name(Test)):
-					self.step()
-					tokens.append(Token(TT_METHOD, Test))
+			if ch == R_Paranth:
+				#right parathesis
+				self.step()
+				tokens.append(Token(TT_BRACK, R_Paranth))
+				continue
+				
+			if ch == L_Curl:
+				#left curly bracket
+				self.step()
+				tokens.append(Token(TT_BRACK, L_Curl))
+				continue
+				
+			if ch == R_Curl:
+				#right curly bracket
+				self.step()
+				tokens.append(Token(TT_BRACK, R_Curl))
+				continue
 			
-			elif ch == '>':
-				#Then ?
-				if (self.build_misc_name(Then)):
-					self.step()
-					tokens.append(Token(TT_SYMBOL, Then))
-			
-			elif ch == ':':
-				#Comparison ?
-				if (self.build_misc_name(Comparison)):
-					self.step()
-					tokens.append(Token(TT_SYMBOL, Comparison))
-			
-			else: 
-				#make a variable name
-				tokens.append(Token(TT_VAR, self.build_var_name()))
+			#make a variable name
+			tokens.append(Token(TT_VAR, self.build_var_name()))
 			
 		return tokens
 
@@ -161,7 +193,7 @@ class Lexer(object):
 	
 	def build_var_name(self):
 		result = ''
-		while self.current_char is not None and not self.current_char.isspace():
+		while self.current_char is not None and not self.current_char.isspace() and not self.current_char in BannedChars:
 			result += self.current_char
 			self.step()
 		return result
@@ -180,10 +212,14 @@ class Interpreter(object):
 
 		self.pos = 0
 		self.current_token = self.tokens[self.pos]
+
+		self.depth = 0 #keep track of the level of { }
+		self.memory_depth = 0
 	
 	def print_tokens(self):
 		for token in self.tokens:
 			print(token)
+		print("\nFinished printing tokens...\n")
 
 	def error(self):
 		raise Exception('Interpreting error')
@@ -194,15 +230,38 @@ class Interpreter(object):
 			self.current_token = Token(TT_EOL, None)
 		else:
 			self.current_token = self.tokens[self.pos]
+	
+	def eat_type(self, type):
+		token = self.current_token
+		if token.type == type:
+			self.step()
+			return token
+		else:
+			self.error()
+		
+	def eat_value(self, value):
+		token = self.current_token
+		if token.value == value:
+			self.step()
+			return token
+		else:
+			self.error()
+	
+	def level_down(self):
+		while self.current_token.value == R_Curl:
+			self.step()
+			self.depth -= 1
+		if self.depth < 0:
+			self.error()
 
 	def execute_tokens(self):
-		while self.current_token is not None and self.current_token.type is not TT_EOL:
+		while self.current_token is not None and self.current_token.value is not None:
 
 			if self.current_token.type == TT_KWORD:
 				#Is a keyword like Var
 				token = self.current_token
 
-				if token.value == Var:
+				if token.value == Var: # 'var i = 6;'
 					self.step()
 					self.make_variable()
 
@@ -211,37 +270,47 @@ class Interpreter(object):
 				#Is a method, check depending what the method does
 				token = self.current_token
 
-				if token.value == Print:
+				if token.value == Print: # 'print("something");'
 					self.step()
+					self.eat_value(L_Paranth)
 					printable = self.make_string()
+					self.eat_value(R_Paranth)
+					self.eat_type(TT_EOL)
 					print(printable)
+					self.level_down()
 				
-				if token.value == Test: 
+				if token.value == If: # 'if(1:=1){print("something")};'
 					self.step()
+					self.eat_value(L_Paranth)
 					if self.make_test():
-						self.step() if self.current_token.value == Then else self.error()
-					else:
-						break
+						self.eat_value(R_Paranth)
+						self.eat_value(L_Curl)
+						self.depth += 1
+					else: 								##################### A REVOIR
+						self.memory_depth = self.depth
+						while self.current_token.value is not R_Curl and self.depth != self.memory_depth:
+							if self.current_token.value == L_Curl:
+								self.depth += 1
+								print(self.depth - self.memory_depth)
+							elif self.current_token.value == R_Curl:
+								self.depth -= 1
+								print(self.depth - self.memory_depth)
+							self.step()
+					self.level_down()
+
 	
 
 
-	def make_variable(self):
-		if self.current_token.type is not TT_VAR:
-			self.error()
-		name = self.current_token.value
-		self.step()
+	def make_variable(self): # 'i = 6;'
+		name = self.eat_type(TT_VAR).value # = 6;
 
-		if self.current_token.value is not Equal:
-			self.error()
-		self.step()
+		self.eat_value(Equal) # 6;
 
 		if self.current_token.type is TT_INT:
 			value = self.make_int()
 		elif self.current_token.type is TT_STRING:
-			value = self.current_token.value
-			self.step()
-		else:
-			self.error()
+			value = self.eat_type(TT_STRING).value
+		self.eat_type(TT_EOL)
 
 		vars.update({name: value})
 
@@ -254,17 +323,15 @@ class Interpreter(object):
 				result += str(self.make_int())
 			elif token.type == TT_STRING:
 				result += token.value
+				self.step()
 			else:
-				if token.value not in vars:
+				if str(token.value) not in vars:
 					self.error()
 				result += str(vars[token.value])
+				self.step()
 			result += ' '
-			self.step()
 
-		if result == '':
-			self.error()
-		else:
-			return result
+		return result
 	
 	def make_int(self):
 		result = self.current_token.value
@@ -296,10 +363,7 @@ class Interpreter(object):
 			self.error()
 
 		#get test:
-		if self.current_token.value == Comparison:
-			self.step()
-		else:
-			self.error()
+		self.eat_value(Comparison)
 
 		#get the second value
 		if self.current_token.type == TT_INT:
@@ -330,12 +394,12 @@ def main():
 	path = input('klank file Path >>> ')
 	with open(path) as f:
 		lines = f.readlines()
+	text = ' '.join(lines)
 
-	for text in lines:
-		lexer = Lexer(text)
-		inter = Interpreter(lexer)
-		inter.print_tokens()
-		inter.execute_tokens()
+	lexer = Lexer(text)
+	inter = Interpreter(lexer)
+	inter.print_tokens()
+	inter.execute_tokens()
 
 	input('\n\nPress enter to finish...')
 
